035104b41e3b69a35059c043b387ada2899b0085
Jakob Kirsch <jakob.kirsch@web.de>
2021-02-10 22:14:38 +0100

    Add base

--- folding/folding.py
+++ folding/folding.py
@@ -1,5 +1,22 @@
-# todo
+import numpy as np, random, math
 
 def fold(acids):
-    atoms = []
-    return atoms
+    # make a virtual space for folding
+    size = len(acids) + 4
+    size -= size % 2
+    size += 1
+    space = np.zeros((size, size, size)).astype(int)
+
+    # fill the space with the actual acids
+    # addressing is [x][y][z]
+
+    # y
+    # | z
+    # |/
+    # 0----x
+    # the acid chain goes from min middle middle to max middle middle
+    middle = math.ceil(size / 2)
+    for i in range(2, len(acids) + 2):
+        space[middle][middle][i] = acids[i - 2]
+
+    return space
---
+++ folding/test.py
@@ -0,0 +1,5 @@
+import folding
+
+print("Test 1: run it :D")
+space = folding.fold([1, 20, 9, 18])
+print(space)
