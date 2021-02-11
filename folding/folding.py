0752a701f08e5599f5a6d3bb4e12bd61afb17c1c
Jakob Kirsch <jakob.kirsch@web.de>
2021-02-11 15:09:46 +0100

    Add plotter

--- folding/folding.py
+++ folding/folding.py
@@ -5,7 +5,7 @@ def fold(acids):
     size = len(acids) + 4
     size -= size % 2
     size += 1
-    space = np.zeros((size, size, size)).astype(int)
+    space = np.zeros((size, size, size))
 
     # fill the space with the actual acids
     # addressing is [x][y][z]
@@ -20,3 +20,26 @@ def fold(acids):
         space[middle][middle][i] = acids[i - 2]
 
     return space
+
+def plot(space):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xticks(range(0, space.shape[0]))
+    ax.set_yticks(range(0, space.shape[0]))
+    ax.set_zticks(range(0, space.shape[0]))
+    for x in range(0, len(space)):
+        for y in range(0, len(space[x])):
+            for z in range(0, len(space[x, y])):
+                t = space[x, y, z]
+                if t > 0:
+                    rng = random.Random()
+                    rng.seed(t)
+                    c = []
+                    for i in range(0, 3):
+                        c.append(rng.randint(0, 256) / 256)
+                    ax.scatter(z, x, -y, zdir='z', c=c)
+    plt.axis('off')
+    plt.show()
--- folding/test.py
+++ folding/test.py
@@ -3,3 +3,6 @@ import folding
 print("Test 1: run it :D")
 space = folding.fold([1, 20, 9, 18])
 print(space)
+
+print("Test 2: plot it")
+folding.plot(space)
