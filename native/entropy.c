4e56c856aed023724d63be93812d603ab75df67a
Jakob Kirsch <jakob.kirsch@web.de>
2021-11-17 15:44:20 +0100

    native: entropy: add section mode

--- native/entropy.c
+++ native/entropy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
 
 #define BLOCK_SIZE 65536
 
@@ -18,25 +20,33 @@ void check(char letter) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	if (argc < 4) {
 		return 1;
 	}
 
 	FILE *fptr = fopen(argv[1], "rb");
-	if (fptr == NULL) {
-		return 1;
+	long start = strtol(argv[2], NULL, 10);
+	long end = strtol(argv[3], NULL, 10);
+
+	if (end == 0) {
+		end = LONG_MAX;
 	}
+
+	long to_read = end - start;
+
+	fseek(fptr, start, SEEK_SET);
 
 	while (1) {
 		int read = fread(&block, 1, sizeof(block), fptr);
-		length = ftell(fptr);
+		length = length + read;
 
-		if (read == 0) {
+		if (read == 0 || to_read == 0) {
 			break;
 		}
 
-		for (int i = 0; i < BLOCK_SIZE; i++) {
+		for (int i = 0; i < BLOCK_SIZE && to_read > 0; i++) {
 			seen[block[i]]++;
+			to_read--;
 		}
 
 		memset(&block, 0, sizeof(block));
@@ -49,8 +59,6 @@ int main(int argc, char *argv[]) {
 	entropy = entropy / 2.0f;
 
 	printf("%f\n", entropy);
-
-	fclose(fptr);
 
 	return 0;
 }
